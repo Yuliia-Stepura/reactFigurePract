e5fa95427cca6d4f49d5698e3bd88d814a84c8a7
Yuliia-Stepura <79403122+Yuliia-Stepura@users.noreply.github.com>
2021-06-13 00:22:53 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Svg, { Circle, Rect, Ellipse, ForeignObject } from 'react-native-svg';
 import { Text, View, ScrollView, StyleSheet, TextInput, Button, CheckBox, ImageBackground} from 'react-native';
 import Constants from 'expo-constants';
 import AppLoader from './assets/AppLoader';
-import setValue from './components/setValue';
+
 class Calculate extends React.Component {
   constructor(props) {
     super();
